242ed2a35d0959c6999b2582e6070818f4afc39f
Rafael Vargas <rafael.vargas@outlook.com>
2022-06-12 16:00:24 -0400

    inclass 1.11 lab done

--- session_01_working_files/inclass_exercises/inclass_1.11_lab.py
+++ session_01_working_files/inclass_exercises/inclass_1.11_lab.py
@@ -4,7 +4,15 @@
 
 import runreport
 
+fname = "../pyku.txt"
 
+fh = open(fname)
+
+
+for line in fh:
+    line_words = line.rstrip().split()
+    first_word = line_words[0].rstrip(",")
+    print(first_word)
 
 
 # Expected Output:
@@ -14,4 +22,3 @@ import runreport
 # Spam
 
 # Please note that there is no comma after the word 'Spam'
-
