bc0c66f1b6afeb8e4a101d5985fdb2b61c1b3f0d
Rafael Vargas <rafael.vargas@outlook.com>
2022-06-12 16:18:30 -0400

    inclass lab 1.12 done

--- session_01_working_files/inclass_exercises/inclass_1.12_lab.py
+++ session_01_working_files/inclass_exercises/inclass_1.12_lab.py
@@ -1,12 +1,23 @@
 # 1.12:  Use a 'for' loop to sum up int/float values in a
 # file.  Sum up the 'volume' values in the file ad_buys.csv.
 
+
 import runreport
 
+fname = "../ad_buys.csv"
+fh = open(fname)
+next(fh)
 
+sum = 0
+
+for line in fh:
+    datetime, buyer_id, seller_id, volume, price = line.rstrip().split(",")
+    volume = int(volume)
+    sum += volume
+
+print(sum)
 
 
 # Expected Output:
 
 # 150
-
